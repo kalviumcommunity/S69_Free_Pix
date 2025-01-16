b3e1d4d1bf38f5140fa8f93cbffdd5f4667896b6
shangesh-tech <shangesh2006@gmail.com>
2025-01-16 15:41:08 +0530

    feat:backend development

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,24 @@
-const express = require('express')
+const express = require("express");
+require("dotenv").config();
+const mongoose = require("mongoose");
 const app = express()
-const port = 3000
+const port = process.env.PORT || 5000;
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+
+
+// DB connection
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to database!");
+    app.listen(port, () => {
+      console.log(`Server is running on port ${process.env.PORT} and http://localhost:${port}`);
+    });
+  })
+  .catch((error) => {
+    console.log("Connection failed!", error);
+  });
